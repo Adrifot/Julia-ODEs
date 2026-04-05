c920b04e637277c15b77d58bcff35cbd4a929c44
Adrian Fota <adrian.mihael.fota@gmail.com>
2026-04-05 10:01:47 +0300

    Add DifferentialEquations.jl solution

--- lotka_volterra.jl
+++ lotka_volterra.jl
@@ -1,29 +1,48 @@
 using Plots
 using Plots.Measures
+using DifferentialEquations
 gr()
 
-α, β, γ, δ = 1, 0.04, 0.01, 0.8
+params = 1, 0.04, 0.01, 0.8
 dt = 0.01
 simtime = 100.0
-nsteps = Int(simtime / dt)
+p0 = 40.0, 3.0
 
-x, y = 40.0, 3.0
-pophistory = [(x, y)] 
+function lotka_volterra_euler(params, p0, dt, simtime)
+    α, β, γ, δ = params
+    nsteps = Int(simtime / dt)
+    
+    # Pre-allocation
+    ox = collect(0:dt:simtime)
+    oy1 = zeros(length(ox))
+    oy2 = zeros(length(ox))
 
-for t in 1:nsteps
-    dx = (α*x - β*x*y) * dt 
-    dy = (γ*x*y - δ*y) * dt
-    
-    global x += dx
-    global y += dy
-    
-    push!(pophistory, (x < 0 ? 0 : x, y < 0 ? 0 : y))
+    oy1[1], oy2[1] = p0
+
+    for i in 1:nsteps
+        x, y = oy1[i], oy2[i]
+        
+        dx = (α*x - β*x*y) * dt 
+        dy = (γ*x*y - δ*y) * dt
+        
+        next_x = x + dx
+        next_y = y + dy
+        
+        oy1[i+1] = max(0.0, next_x)
+        oy2[i+1] = max(0.0, next_y)
+    end
+
+    return ox, oy1, oy2
 end
 
+function lotka_volterra(du, u, p, t)
+    x, y = u 
+    α, β, γ, δ = p 
+    du[1] = α*x - β*x*y
+    du[2] = γ*x*y - δ*y
+end
 
-ox = range(0, step=dt, length=length(pophistory))
-oy1 = first.(pophistory)
-oy2 = last.(pophistory)
+ox, oy1, oy2 = lotka_volterra_euler(params, p0, dt, simtime)
 
 p1 = plot(
     ox, [oy1 oy2], 
@@ -51,6 +70,28 @@ combined_plot = plot(
 )
 
 display(combined_plot)
+
+println("Press Enter...")
+readline()
+
+prob = ODEProblem(lotka_volterra, collect(p0), simtime, collect(params))
+sol = solve(prob)
+
+p3 = plot(sol, title="Lotka-Volterra Solution", xlabel="Time", ylabel="Population",
+            label=["Prey (x)" "Predator (y)"])
+
+p4 = plot(sol, vars=(1, 2), title="Phase Portrait", xlabel="Prey", ylabel="Predator",
+            label = "Orbit")
+
+combined_plot2 = plot(
+    p3, p4, 
+    layout = (1, 2),
+    size=(1200, 500),
+    margin = 5mm,      
+    left_margin = 10mm
+)
+
+display(combined_plot2)
 
 println("Press Enter...")
 readline()
